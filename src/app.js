6a5471fcd5cd4b52aaebbc3b37e08207655687bf
hlnvrnt <helene.vernet@live.fr>
2023-11-27 13:18:51 +0100

    challenge

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+require("dotenv").config();
 const app = express();
 
 const movieControllers = require("./controllers/movieControllers");
@@ -7,4 +7,13 @@ const movieControllers = require("./controllers/movieControllers");
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 
+
+const usersControllers = require("./controllers/usersControllers");
+
+app.get("/api/users", usersControllers.getUsers);
+app.get("/api/users/:id", usersControllers.getUsersById);
+
 module.exports = app;
+
+
+const port = process.env.APP_PORT;
