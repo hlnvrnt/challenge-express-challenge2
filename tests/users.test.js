d15d433595d2cd6e47d5eee1a122945812effcdd
hlnvrnt <helene.vernet@live.fr>
2023-11-28 16:01:35 +0100

    valider saisie

--- tests/users.test.js
+++ tests/users.test.js
@@ -93,7 +93,7 @@ describe("POST /api/users", () => {
       .post("/api/users")
       .send(userWithMissingProps);
 
-    expect(response.status).toEqual(500);
+    expect(response.status).toEqual(422);
   });
 });
 
@@ -157,7 +157,7 @@ describe("PUT /api/users/:id", () => {
       .put(`/api/users/1`)
       .send(userWithMissingProps);
 
-    expect(response.status).toEqual(500);
+    expect(response.status).toEqual(422);
 });
 
 it("should return no user", async () => {
